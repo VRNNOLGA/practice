316b70948c1e808b32ac6b65cd2c98974e6bbe54
VRNNOLGA <olga-pekin@yandex.ru>
2023-07-22 22:57:42 +0200

    new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import image from './plan.jpg';
 import './App.css';
+import { List } from './List';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <img src={image} width= "100%"  alt='plan'/>
+      <List />
     </div>
   );
 }
-
 export default App;
+
+
